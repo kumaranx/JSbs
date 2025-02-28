e6d6a598c58d30b556ce8232729ab27104c3b3f0
Kumaran <kumaran.0q@gmail.com>
2025-02-28 14:27:40 +0530

    upto logical operators
    Block & Function Scoped, Arithmetic Operators & Coercion, Type Conversions, Comparison Operators, Logical Operators

--- script.js
+++ script.js
@@ -20,11 +20,57 @@
 
 // --- Variables
 
-var greetingMsg = 'hola'
-greetingMsg = 'amingo'
+// var greetingMsg = 'hola'
+// greetingMsg = 'amingo'
 
-let x = 0
-x += 1
+// let x = 0
+// x += 1
 
-const RPM = 14000  // arrays int const are mutable(can be changed) but cannot reassign new array to this variable
+// const RPM = 14000  // arrays int const are mutable(can be changed) but cannot reassign new array to this variable
+
+// // --- Block & Function Scoped
+
+// if (true) {
+//   var x = 4  // var is function scoped, so can be accessed anywhere, var are also hoisted on top of the function
+
+//   let y = 0   // let & const are block scoped so won't able to access them outside of the block
+//   const z = 1
+//   console.log(y)
+// }
+// console.log(x)
+// // console.log(z)
+
+// --- Conversions
+
+// Number()
+// parseInt()
+// parseFloat()
+// String()
+// .toString()
+
+// --- Comparison Operators
+
+/*
+==
+===
+!=
+!==
+<
+>
+<=
+>=
+
+*/
+
+// // -- Logical Operators
+
+// /*
+// &&
+// ||
+// |
+// */
+
+// console.log(-1 > 0 && 'hola')
+
+// --- Conditionals (if, else if, else)
 

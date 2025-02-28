1917438d0d6a792ff9d4836921ba8d6aae2d9af3
Kumaran <kumaran.0q@gmail.com>
2025-02-28 12:21:46 +0530

    upto var
    user input, data types, variables

---
+++ script.js
@@ -0,0 +1,30 @@
+// console.error('Oops')
+// console.warn('Oh Oh')
+
+// // --- User Input in Node
+// const prompt = require('prompt-sync')()
+
+// const i = prompt('type something')
+
+// console.log(i)
+
+// // --- Data Types
+
+// string
+// number
+// boolean
+// undefined
+// null
+
+// BigInt  // used to store big integers which cant be stored in number
+
+// --- Variables
+
+var greetingMsg = 'hola'
+greetingMsg = 'amingo'
+
+let x = 0
+x += 1
+
+const RPM = 14000  // arrays int const are mutable(can be changed) but cannot reassign new array to this variable
+
